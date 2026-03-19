9602b8cc447a2646cd47b3226928525313737474
CoToft <cody.toft@ndsu.edu>
2026-03-18 23:05:22 -0500

    Milestone 10

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
   templateUrl: './details.html',
   styleUrl: './details.css'
 })
-export class Details {}
+export class Details {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
+
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+}
