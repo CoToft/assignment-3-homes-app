bcaa717d87c28a4d8ca3718e47af9d0adf996762
CoToft <cody.toft@ndsu.edu>
2026-03-18 22:12:00 -0500

    Milestone 2

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from '../housing-location/housing-location';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [HousingLocation],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
 export class Home {
 
