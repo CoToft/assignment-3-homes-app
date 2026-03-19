1b57afb46a64780688f420146c67f31d11ca11a5
CoToft <cody.toft@ndsu.edu>
2026-03-18 22:36:03 -0500

    Milestone 7

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -10,14 +10,36 @@ import { HousingLocation } from '../housinglocation';
   styleUrl: './home.css'
 })
 export class Home {
-  housingLocation: HousingLocation = {
-    id: 0,
-    name: 'Acme Fresh Start Housing',
-    city: 'Chicago',
-    state: 'IL',
-    photo: 'https://angular.dev/assets/images/tutorials/common/bernard-hermant-CLKGGwIBTaY-unsplash.jpg',
-    availableUnits: 4,
-    wifi: true,
-    laundry: true
-  };
+  housingLocationList: HousingLocation[] = [
+    {
+      id: 0,
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: 'https://angular.dev/assets/images/tutorials/common/bernard-hermant-CLKGGwIBTaY-unsplash.jpg',
+      availableUnits: 4,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 1,
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica',
+      state: 'CA',
+      photo: 'https://angular.dev/assets/images/tutorials/common/brandon-griggs-wR11KBaB86U-unsplash.jpg',
+      availableUnits: 0,
+      wifi: false,
+      laundry: true
+    },
+    {
+      id: 2,
+      name: 'Warm Beds Housing Support',
+      city: 'Juneau',
+      state: 'AK',
+      photo: 'https://angular.dev/assets/images/tutorials/common/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg',
+      availableUnits: 1,
+      wifi: false,
+      laundry: false
+    }
+  ];
 }
