5a5d8c40349208bacb1a416574751482087787d9
CoToft <cody.toft@ndsu.edu>
2026-03-18 22:24:08 -0500

    Milestone 3

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { HousingLocation } from '../housing-location/housing-location';
+import { HousingLocation as HousingLocationComponent } from '../housing-location/housing-location';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HousingLocation],
+  imports: [HousingLocationComponent],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+  housingLocation: HousingLocation = {
+    id: 0,
+    name: 'Acme Fresh Start Housing',
+    city: 'Chicago',
+    state: 'IL',
+    photo: 'https://angular.dev/assets/images/tutorials/common/bernard-hermant-CLKGGwIBTaY-unsplash.jpg',
+    availableUnits: 4,
+    wifi: true,
+    laundry: true
+  };
 }
